0d1c087c1a9b7ac45535014add2138a517af8895
ghouldedinside <johannkrauser182@gmail.com>
2023-04-07 23:02:47 +0300

    first ver

---
+++ src/Header/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div className="Header-site-Header">
+        <h1>Кафе и рестораны </h1>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/Main/Main.js
@@ -0,0 +1,17 @@
+import articles from "../articles.json";
+import "./Main.css";
+function Main() {
+    return(
+        <div className="Main">
+            {articles.map(item=>
+            <div>
+                <img src={item.img} alt="img" />
+                <h2>{item.title}</h2>
+                <p>{item.body}</p>
+            </div>)}
+            
+        </div>
+    );
+}
+
+export default Main;
