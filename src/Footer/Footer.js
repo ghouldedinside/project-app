ee1dd49e6635cb4faf89336ef58f7d78d498c588
ghouldedinside <johannkrauser182@gmail.com>
2023-04-12 21:33:13 +0300

    some changes

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <div className="Footer-site-Footer">
+      <img src="images/icon-1.png" alt="icon1" />
+      <img src="images/icon-2.webp" alt="icon2" />
+      <img src="images/icon-3.webp" alt="icon3" />
+      <img src="images/icon-4.png" alt="icon4" />
+    </div>
+  );
+}
+
+export default Footer;
